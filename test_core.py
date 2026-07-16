dcc5bdaf5f53f9bc679628c4e1393579c661f1f3
LucasBelizote <124172146+LucasBelizote@users.noreply.github.com>
2026-07-15 23:01:37 -0300

    Update test_core.py

--- test_core.py
+++ test_core.py
@@ -15,17 +15,17 @@ def setup_e_teardown():
 def test_fluxo_cadastro_e_login():
     core = SistemaLogisticaCore()
     
-    # Testa Cadastro
+    
     sucesso, msg = core.cadastrar_usuario("Lucas Operador", "lucas@techflow.com", "12345")
     assert sucesso is True
     assert len(core.dados['usuarios']) == 1
     
-    # Testa Login Válido
+    
     login_ok, user = core.fazer_login("lucas@techflow.com", "12345")
     assert login_ok is True
     assert user['username'] == "Lucas Operador"
     
-    # Testa Login Inválido
+   
     login_errado, msg_err = core.fazer_login("lucas@techflow.com", "senha_errada")
     assert login_errado is False
 
@@ -34,7 +34,7 @@ def test_crud_tarefas_com_mudanca_de_escopo():
     core.cadastrar_usuario("Lucas Operador", "lucas@techflow.com", "12345")
     core.fazer_login("lucas@techflow.com", "12345")
     
-    # Teste: Create (Criar Rota)
+  
     sucesso, msg = core.criar_tarefa("Rota de Entrega - Zona Sul", "Levar container 4", "Alta")
     assert sucesso is True
     
@@ -42,13 +42,12 @@ def test_crud_tarefas_com_mudanca_de_escopo():
     assert len(tarefas) == 1
     tarefa = tarefas[0]
     assert tarefa['titulo'] == "Rota de Entrega - Zona Sul"
-    assert tarefa['motorista'] == "Não atribuído" # Valor padrão da mudança de escopo
+    assert tarefa['motorista'] == "Não atribuído" 
     
-    # Teste: Update Status
     core.atualizar_status_tarefa(tarefa['id'], "in_progress")
     assert core.listar_tarefas_usuario()[0]['status'] == "in_progress"
     
-    # Teste: Mudança de Escopo (Atribuir Motorista)
+    
     core.atribuir_motorista_tarefa(tarefa['id'], "Carlos Silva")
     assert core.listar_tarefas_usuario()[0]['motorista'] == "Carlos Silva"
     
